0f29408883bb53ac19d38d685aeaf7ef9eecb010
Appnovate <appnovate789@gmail.com>
2024-02-06 18:35:27 +0530

    fix:cors defalut

--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -5,9 +5,11 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
-      headers: '*',
-      origin: ['https://silly-nightingale.142-93-209-108.plesk.page']
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      origin: '*',
+      credentials: true,
+      keepHeaderOnError: false,
     }
   },
   'strapi::logger',

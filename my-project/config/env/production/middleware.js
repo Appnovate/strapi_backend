339f36c40ad8445557b252c24699dc8148cb34c6
Appnovate <appnovate789@gmail.com>
2024-02-16 12:30:31 +0530

    fix:middlewares

--- my-project/config/env/production/middleware.js
+++ my-project/config/env/production/middleware.js
@@ -2,14 +2,14 @@ module.exports = [
     "strapi::errors",
     "strapi::security",
      {
-      name: 'strapi::cors',
-      config: {
-        origin: ['*'],
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-        keepHeaderOnError: true,
-      }
-     },
+        name: 'strapi::cors',
+        config: {
+          enabled: true,
+          headers: '*',
+          origin: ['https://silly-nightingale.142-93-209-108.plesk.page','https://exciting-blackburn.142-93-209-108.plesk.page'],
+
+        }
+      },
     "strapi::poweredBy",
     "strapi::logger",
     "strapi::query",
--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::poweredBy',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://silly-nightingale.142-93-209-108.plesk.page','https://exciting-blackburn.142-93-209-108.plesk.page'],
+
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
